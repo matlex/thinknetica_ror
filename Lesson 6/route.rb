ffb4d3eef494154fec233d165280c15466d34764
Matvey Ustinov <mat.ustinov@gmail.com>
2018-07-03 17:25:25 +0000

    Задание к уроку "Исключения": добавил валидацию станций при создании маршрута.

--- Lesson 6/route.rb
+++ Lesson 6/route.rb
@@ -1,12 +1,28 @@
+require_relative 'station'
+require_relative 'custom_errors'
+
 class Route
   include InstanceCounter
+  include CustomErrors
 
   attr_reader :stations, :title
 
+  CORRECT_STATION_TYPE = Station
+
   def initialize(start_station, end_station)
     @stations = [start_station, end_station]
+    validate!
+
     @title = "Route from #{@stations.first.name} to #{@stations.last.name}"
     register_instance
+  end
+
+  def valid?
+    begin
+      validate!
+    rescue
+      false
+    end
   end
 
   def show_stations
@@ -29,5 +45,11 @@ class Route
 
   def count_stations
     @stations.size
+  end
+
+  def validate!
+    raise ValidationError, "Start station should be a '#{ CORRECT_STATION_TYPE }' class" if !stations.first.is_a?(CORRECT_STATION_TYPE) || stations.first.nil?
+    raise ValidationError, "End station should be a '#{ CORRECT_STATION_TYPE }' class" if !stations.last.is_a?(correct_station_type) || stations.last.nil?
+    true
   end
 end
