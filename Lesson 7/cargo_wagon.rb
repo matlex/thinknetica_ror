0e4b80d351a43ba674fb381d69f3396c2940e28e
Matvey Ustinov <mat.ustinov@gmail.com>
2018-07-17 13:07:19 +0000

    Задание к уроку proc, block, lambda: Модифицировать грузовые вагоны, добавить объем и методы работающие с объемом. Модифицировать пассажирские вагоны - добавить методы работающие с местами в магонах.

--- Lesson 7/cargo_wagon.rb
+++ Lesson 7/cargo_wagon.rb
@@ -1,4 +1,22 @@
 require_relative 'wagon'
 
 class CargoWagon < Wagon
+  def initialize(total_volume)
+    @total_volume = total_volume
+    @reserved_volume = 0
+  end
+
+  def reserve_volume(amount)
+    if @reserved_volume < @total_volume
+      @reserved_volume += amount
+    end
+  end
+
+  def total_reserved_volume
+    @reserved_volume
+  end
+
+  def total_free_volume
+    @total_volume - @reserved_volume
+  end
 end
--- Lesson 7/passenger_wagon.rb
+++ Lesson 7/passenger_wagon.rb
@@ -1,4 +1,22 @@
 require_relative 'wagon'
 
 class PassengerWagon < Wagon
+  def initialize(total_seats)
+    @total_seats = total_seats
+    @reserved_seats = 0
+  end
+
+  def reserve_seat!
+    if @reserved_seats < @total_seats
+      @reserved_seats += 1
+    end
+  end
+
+  def total_reserved_seats
+    @reserved_seats
+  end
+
+  def total_free_seats
+    @total_seats - @reserved_seats
+  end
 end
